069f34f91a44badbdca0db5aacfec960071a4d9a
Dragon Lin <Dragon Lin>
2016-08-06 23:09:47 +0800

    modify original email code

--- Week2-Basic Structured Query Language/emaildb.py
+++ Week2-Basic Structured Query Language/emaildb.py
@@ -9,33 +9,34 @@ DROP TABLE IF EXISTS Counts''')
 cur.execute('''
 CREATE TABLE Counts (email TEXT, count INTEGER)''')
 
-fname = input('Enter file name: ')
-if (len(fname) < 1): fname = 'mbox.txt'
+fname = 'mbox.txt'
 fh = open(fname)
 for line in fh:
-    if not line.startswith('From: '): continue
+    if not line.startswith('From: '):
+        continue
     pieces = line.split()
     email = pieces[1]
     print(email)
-    cur.execute('SELECT count FROM Counts WHERE email = ? ', (email, ))
+    sql = "SELECT count FROM Counts WHERE email = '{mail}'"
+    cur.execute(sql.format(mail=email))
     row = cur.fetchone()
     if row is None:
-        cur.execute('''INSERT INTO Counts (email, count) 
-                VALUES ( ?, 1 )''', (email,))
-    else : 
-        cur.execute('UPDATE Counts SET count=count+1 WHERE email = ?', 
-            (email, ))
-    # This statement commits outstanding changes to disk each 
-    # time through the loop - the program can be made faster 
-    # by moving the commit so it runs only after the loop completes
+        sql = (
+            "INSERT INTO Counts (email, count)"
+            "VALUES ( '{mail}', 1 )"
+            )
+        cur.execute(sql.format(mail=email))
+    else:
+        sql = "UPDATE Counts SET count=count+1 WHERE email = '{mail}'"
+        cur.execute(sql.format(mail=email))
+
     conn.commit()
 
-# https://www.sqlite.org/lang_select.html
 sqlstr = 'SELECT email, count FROM Counts ORDER BY count DESC LIMIT 10'
 
 print("Counts:")
-for row in cur.execute(sqlstr) :
+for row in cur.execute(sqlstr):
     print(str(row[0]), row[1])
 
 cur.close()
-
+print('email check finish!')
