e4e96c7ef2d34066fb9b641ad5b236d35625b730
Dragon Lin <Dragon Lin>
2016-08-13 16:45:42 +0800

    assignment - Tracks

--- Week3-Multi-Table Database/tracks.py
+++ Week3-Multi-Table Database/tracks.py
@@ -9,30 +9,33 @@ cur.executescript('''
 DROP TABLE IF EXISTS Artist;
 DROP TABLE IF EXISTS Album;
 DROP TABLE IF EXISTS Track;
-
+DROP TABLE IF EXISTS Genre;
 CREATE TABLE Artist (
     id  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
     name    TEXT UNIQUE
 );
-
 CREATE TABLE Album (
     id  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
     artist_id  INTEGER,
     title   TEXT UNIQUE
 );
-
 CREATE TABLE Track (
     id  INTEGER NOT NULL PRIMARY KEY 
         AUTOINCREMENT UNIQUE,
     title TEXT  UNIQUE,
     album_id  INTEGER,
+    genre_id    INTEGER,
     len INTEGER, rating INTEGER, count INTEGER
+);
+CREATE TABLE Genre(
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    name TEXT
 );
 ''')
 
 
-fname = raw_input('Enter file name: ')
-if ( len(fname) < 1 ) : fname = 'Library.xml'
+fname = 'Library.xml'
+
 
 # <key>Track ID</key><integer>369</integer>
 # <key>Name</key><string>Another One Bites The Dust</string>
@@ -40,16 +43,18 @@ if ( len(fname) < 1 ) : fname = 'Library.xml'
 def lookup(d, key):
     found = False
     for child in d:
-        if found : return child.text
-        if child.tag == 'key' and child.text == key :
+        if found:
+            return child.text
+        if child.tag == 'key' and child.text == key:
             found = True
     return None
 
 stuff = ET.parse(fname)
 all = stuff.findall('dict/dict/dict')
-print 'Dict count:', len(all)
+print('Dict count: {0}'.format(len(all)))
 for entry in all:
-    if ( lookup(entry, 'Track ID') is None ) : continue
+    if (lookup(entry, 'Track ID') is None):
+        continue
 
     name = lookup(entry, 'Name')
     artist = lookup(entry, 'Artist')
@@ -57,25 +62,26 @@ for entry in all:
     count = lookup(entry, 'Play Count')
     rating = lookup(entry, 'Rating')
     length = lookup(entry, 'Total Time')
+    genre = lookup(entry, 'Genre')
 
-    if name is None or artist is None or album is None : 
+    if name is None or artist is None or album is None or genre is None:
         continue
 
-    print name, artist, album, count, rating, length
+    print('{name}, {artist}, {album}, {count}, {rating}, {length}'.format(**locals()))
 
-    cur.execute('''INSERT OR IGNORE INTO Artist (name) 
-        VALUES ( ? )''', ( artist, ) )
-    cur.execute('SELECT id FROM Artist WHERE name = ? ', (artist, ))
+    cur.execute("INSERT OR IGNORE INTO Artist (name) VALUES ( '{}' )".format(artist))
+    cur.execute("SELECT id FROM Artist WHERE name = '{}'".format(artist))
     artist_id = cur.fetchone()[0]
 
-    cur.execute('''INSERT OR IGNORE INTO Album (title, artist_id) 
-        VALUES ( ?, ? )''', ( album, artist_id ) )
+    cur.execute("INSERT OR IGNORE INTO Album (title, artist_id) VALUES ( ?, ? )", (album, artist_id))
     cur.execute('SELECT id FROM Album WHERE title = ? ', (album, ))
     album_id = cur.fetchone()[0]
 
+    cur.execute("INSERT OR IGNORE INTO Genre (name) VALUES (?)", (genre, ))
+    cur.execute('SELECT id FROM Genre WHERE name = ? ', (genre, ))
+    genre_id = cur.fetchone()[0]
+
     cur.execute('''INSERT OR REPLACE INTO Track
-        (title, album_id, len, rating, count) 
-        VALUES ( ?, ?, ?, ?, ? )''', 
-        ( name, album_id, length, rating, count ) )
+        (title, album_id, genre_id, len, rating, count) VALUES ( ?, ?, ?, ?, ?, ? )''', (name, album_id, genre_id, length, rating, count))
 
     conn.commit()
