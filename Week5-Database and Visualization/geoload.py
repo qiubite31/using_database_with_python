4ad98701bfc5230ef2fbf4dcfc22e91aa362b7aa
Dragon Lin <Dragon Lin>
2016-08-28 11:25:08 +0800

    clean code and convert python2 to python3

--- Week5-Database and Visualization/geoload.py
+++ Week5-Database and Visualization/geoload.py
@@ -1,4 +1,5 @@
-import urllib
+from urllib.request import urlopen
+from urllib.parse import urlencode
 import sqlite3
 import json
 import time
@@ -18,42 +19,43 @@ cur = conn.cursor()
 cur.execute('''
 CREATE TABLE IF NOT EXISTS Locations (address TEXT, geodata TEXT)''')
 
-fh = open("where.data")
+fh = open("where.data", encoding='utf-8')
 count = 0
 for line in fh:
-    if count > 200 : break
+    if count > 200:
+        break
     address = line.strip()
-    print ''
-    cur.execute("SELECT geodata FROM Locations WHERE address= ?", (buffer(address), ))
+    print('')
+    cur.execute("SELECT geodata FROM Locations WHERE address= ?", (memoryview(address.encode()),))
 
     try:
         data = cur.fetchone()[0]
-        print "Found in database ",address
+        print("Found in database ", address)
         continue
     except:
         pass
 
-    print 'Resolving', address
-    url = serviceurl + urllib.urlencode({"sensor":"false", "address": address})
-    print 'Retrieving', url
-    uh = urllib.urlopen(url, context=scontext)
-    data = uh.read()
-    print 'Retrieved',len(data),'characters',data[:20].replace('\n',' ')
+    print('Resolving', address)
+    url = serviceurl + urlencode({"sensor": "false", "address": address})
+    print('Retrieving', url)
+    uh = urlopen(url, context=scontext)
+    data = uh.read().decode('utf-8')
+    print('Retrieved', len(data), 'characters', data[:20].replace('\n', ' '))
     count = count + 1
-    try: 
+    try:
         js = json.loads(str(data))
         # print js  # We print in case unicode causes an error
-    except: 
+    except:
         continue
 
-    if 'status' not in js or (js['status'] != 'OK' and js['status'] != 'ZERO_RESULTS') : 
-        print '==== Failure To Retrieve ===='
-        print data
+    if 'status' not in js or (js['status'] != 'OK' and js['status'] != 'ZERO_RESULTS'):
+        print('==== Failure To Retrieve ====')
+        print(data)
         break
 
-    cur.execute('''INSERT INTO Locations (address, geodata) 
-            VALUES ( ?, ? )''', ( buffer(address),buffer(data) ) )
-    conn.commit() 
+    cur.execute('''INSERT INTO Locations (address, geodata)
+            VALUES ( ?, ? )''', (memoryview(address.encode()), memoryview(data.encode())))
+    conn.commit()
     time.sleep(1)
 
-print "Run geodump.py to read the data from the database so you can visualize it on a map."
+print("Run geodump.py to read the data from the database so you can visualize it on a map.")
