c5434f6d49cc86bde19576e607c1785a6b78d122
Dragon Lin <Dragon Lin>
2016-08-28 11:51:10 +0800

    clean code and convert to python3

--- Week5-Database and Visualization/geodump.py
+++ Week5-Database and Visualization/geodump.py
@@ -1,31 +1,36 @@
-import sqlite3
 import json
-import codecs
+import sqlite3
 
 conn = sqlite3.connect('geodata.sqlite')
 cur = conn.cursor()
 
 cur.execute('SELECT * FROM Locations')
-fhand = codecs.open('where.js','w', "utf-8")
+fhand = open('where.js', 'w', encoding='utf-8')
 fhand.write("myData = [\n")
 count = 0
-for row in cur :
-    data = str(row[1])
-    try: js = json.loads(str(data))
-    except: continue
+for row in cur:
+    data = row[1].decode()
+    # print(data)
+    try:
+        js = json.loads(str(data))
+    except:
+        continue
 
-    if not('status' in js and js['status'] == 'OK') : continue
+    if not('status' in js and js['status'] == 'OK'):
+        continue
 
-    lat = js["results"][0]["geometry"]["location"]["lat"]
-    lng = js["results"][0]["geometry"]["location"]["lng"]
-    if lat == 0 or lng == 0 : continue
+    lat = js['results'][0]['geometry']['location']['lat']
+    lng = js['results'][0]['geometry']['location']['lng']
+    if lat == 0 or lng == 0:
+        continue
     where = js['results'][0]['formatted_address']
-    where = where.replace("'","")
-    try :
-        print where, lat, lng
+    where = where.replace("'", "")
+    try:
+        print(where, lat, lng)
 
         count = count + 1
-        if count > 1 : fhand.write(",\n")
+        if count > 1:
+            fhand.write(",\n")
         output = "["+str(lat)+","+str(lng)+", '"+where+"']"
         fhand.write(output)
     except:
@@ -34,6 +39,5 @@ for row in cur :
 fhand.write("\n];\n")
 cur.close()
 fhand.close()
-print count, "records written to where.js"
-print "Open where.html to view the data in a browser"
-
+print(count, "records written to where.js")
+print("Open where.html to view the data in a browser")
